908c18581d4a5c396aa504cbf835bc2f27f5cf55
陈海天 <2016010106@secoder.net>
2019-10-27 12:27:25 +0800

    maybe finished base ram?

--- thinpad_top.srcs/sources_1/new/thinpad_top.sv
+++ thinpad_top.srcs/sources_1/new/thinpad_top.sv
@@ -212,4 +212,62 @@ module thinpad_top(
 // /* =========== Demo code end =========== */
 
 
+assign ext_ram_ce_n = 1'b1;
+assign ext_ram_oe_n = 1'b1;
+assign ext_ram_we_n = 1'b1;
+assign uart_rdn = 1'b1;
+assign uart_wrn = 1'b1;
+assign base_ram_be_n = 4'b0;
+
+enum {RECEIVE, WRITE, READ, IDLE} state;
+
+logic[19:0] base_ram_addr_end;
+logic[31:0] bus_data_to_write;
+
+assign base_ram_data = bus_data_to_write;
+
+always_ff @(posedge clock_btn or posedge reset_btn) begin
+    if(reset_btn) begin
+        state <= RECEIVE;
+        base_ram_addr <= dip_sw[19:0];
+        base_ram_addr_end <= dip_sw[19:0] + 9;
+    end else begin
+        case (state)
+            RECEIVE: begin
+                bus_data_to_write <= dip_sw;
+                leds <= dip_sw[15:0];
+                base_ram_we_n <= 0;
+                is_writing <= 1;
+                state <= WRITE;
+            end
+            WRITE: begin
+                if (base_ram_addr == base_ram_addr_end) begin
+                    leds <= {base_ram_addr-9}[15:0];
+                    base_ram_addr <= base_ram_addr - 9;
+                    base_ram_oe_n <= 0;
+                    base_ram_we_n <= 1;
+                    is_writing <= 0;
+                    state <= READ;
+                end else begin
+                    base_ram_addr <= base_ram_addr + 1;
+                    leds <= {base_ram_addr+1}[15:0];
+                    base_ram_we_n <= 1;
+                    state <= RECEIVE;
+                end
+            end
+            READ: begin
+                if (base_ram_addr == base_ram_addr_end) begin
+                    leds <= 0;
+                    state <= IDLE;
+                end else begin
+                    leds <= base_ram_data[15:0];
+                    base_ram_addr <= base_ram_addr + 1;
+                    state <= READ;
+                end
+            end
+            default : /* default */;
+        endcase
+    end
+end
+
 endmodule
